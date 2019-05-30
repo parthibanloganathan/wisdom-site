19b8181453265e8e02ce6871375de79b5b2b9f16
Parthiban Loganathan <parthibanloganathan@users.noreply.github.com>
2019-05-30 11:09:41 -0700

    fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,4 +1,5 @@
 const proxy = require('http-proxy-middleware');
+require('dotenv').config();
 
 // See https://facebook.github.io/create-react-app/docs/proxying-api-requests-in-development
 // See https://github.com/chimurai/http-proxy-middleware/blob/master/recipes/context-matching.md
@@ -8,7 +9,12 @@ var landingPageFilter = function (pathname, req) {
     return pathname.match('^\/$') && req.method === 'GET';
 };
 
+var serverUrl = 'https://wisdom-site-server.herokuapp.com/';
+if (process.env.DEV === true) {
+    serverUrl = 'http://localhost:3001/';
+}
+
 module.exports = function (app) {
-    app.use(proxy('/api', { target: 'http://localhost:3001/' }));
-    app.use(proxy(landingPageFilter, { target: 'http://localhost:3001/' }));
+    app.use(proxy('/api', { target: serverUrl }));
+    app.use(proxy(landingPageFilter, { target: serverUrl }));
 };
