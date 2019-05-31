42fc9c2f1ee66240ec985073ef1bf22eb5c3066d
Parthiban Loganathan <parthibanloganathan@users.noreply.github.com>
2019-05-31 11:01:01 -0700

    fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,22 +1,22 @@
-const proxy = require('http-proxy-middleware');
-require('dotenv').config();
+// const proxy = require('http-proxy-middleware');
+// require('dotenv').config();
 
-// See https://facebook.github.io/create-react-app/docs/proxying-api-requests-in-development
-// See https://github.com/chimurai/http-proxy-middleware/blob/master/recipes/context-matching.md
+// // See https://facebook.github.io/create-react-app/docs/proxying-api-requests-in-development
+// // See https://github.com/chimurai/http-proxy-middleware/blob/master/recipes/context-matching.md
 
-// Matches landing page at / alone
-var landingPageFilter = function (pathname, req) {
-    return pathname.match('^\/$') && req.method === 'GET';
-};
+// // Matches landing page at / alone
+// var landingPageFilter = function (pathname, req) {
+//     return pathname.match('^\/$') && req.method === 'GET';
+// };
 
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
-var serverUrl = 'https://wisdom-site-server.herokuapp.com/';
-if (process.env.NODE_ENV === 'development') {
-    serverUrl = 'http://localhost:3001/';
-}
+// var serverUrl = 'https://wisdom-site-server.herokuapp.com/';
+// if (process.env.NODE_ENV === 'development') {
+//     serverUrl = 'http://localhost:3001/';
+// }
 
-module.exports = function (app) {
-    app.use(proxy('/api', { target: serverUrl }));
-    app.use(proxy(landingPageFilter, { target: serverUrl }));
-};
+// module.exports = function (app) {
+//     app.use(proxy('/api', { target: serverUrl }));
+//     app.use(proxy(landingPageFilter, { target: serverUrl }));
+// };
