0983a6f8ab8388837aae086e2fa702c250b1ad31
Parthiban Loganathan <parthibanloganathan@users.noreply.github.com>
2019-05-31 11:18:48 -0700

    tset

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/', indexRouter);
 
